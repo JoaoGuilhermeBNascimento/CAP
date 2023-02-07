d9694ad5b55556c68890d6dceb72b529b8f4afa0
João Guilherme B. do Nascimento <joaoguilhermebn@hotmail.com>
2023-02-07 17:33:49 -0300

    done

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -5,24 +5,21 @@ export function ModalCards() {
     <Container>
       <div className="box">
 
-        <Link href="http://www.cap.seduh.df.gov.br/wp-content/uploads/2020/03/CHECK-LIST%E2%80%93VIABILIDADE-LEGAL.pdf"  target="_blank">
-        Checklist Viabilidade Legal
+        <Link href="https://www.youtube.com/watch?v=GhnhxCjEtPM"  target="_blank">
+         Vídeo tutorial - Nova Solicitação (CAP-Web)
         </Link>
-        <Link href="http://www.cap.seduh.df.gov.br/wp-content/uploads/2020/03/CHECK-LIST-ESTUDO-PR%C3%89VIO.pdf" target="_blank">
-        Checklist Análise Concomitante
+        <Link href="https://www.youtube.com/watch?v=9ZPVfiSsz8Q" target="_blank">
+         Vídeo tutorial - Cumprimento de Exigência (CAP-Web)
         </Link>
-        <Link href="http://www.cap.seduh.df.gov.br/wp-content/uploads/2020/03/CHECK-LIST-AN%C3%81LISE-CONCOMITANTE.pdf" target="_blank">
-        Checklist Análise Conjunta
+        <Link href="https://www.youtube.com/watch?v=cVBWqzQZ8Sc" target="_blank">
+         Vídeo tutorial - Solicitação de Taxas (CAP-Web)
         </Link>
         <Link href="http://www.cap.seduh.df.gov.br/wp-content/uploads/2020/03/CHECK-LIST%E2%80%93VIABILIDADE-LEGAL.pdf" target="_blank">
         Checklist Viabilidade Legal
         </Link>
-        <Link href="http://www.cap.seduh.df.gov.br/wp-content/uploads/2020/03/CHECK-LIST-ESTUDO-PR%C3%89VIO.pdf">
-        Checklist Análise Concomitante
-        </Link>
-        <Link href="http://www.cap.seduh.df.gov.br/wp-content/uploads/2020/03/CHECK-LIST-AN%C3%81LISE-CONCOMITANTE.pdf" target="_blank">
-        Checklist Análise Conjunta
-        </Link>
+        <Link href="https://www.youtube.com/watch?v=DbROPzNFngE" target="_blank">
+        Vídeo tutorial - Agendamento de Reunião com o Analista (CAP-Web)        </Link>
+
       </div>
 
     </Container>
--- src/components/ModalCode/index.js
+++ src/components/ModalCode/index.js
@@ -15,6 +15,12 @@ export function ModalCode() {
         <Link href="http://www.cap.seduh.df.gov.br/wp-content/uploads/2019/08/I_Requerimento_Padrao_COE_antigo.doc" target="_blank">
         Requerimento Padrão (COE ANTIGO)
         </Link>
+        <Link href="http://www.cap.seduh.df.gov.br/wp-content/uploads/2022/03/Texto-comparativo-entre-a-Lei-o-6.138_2018-e-o-Decreto-43.056_2022.pdf" target="_blank">
+        Texto comparativo entre a Lei º 6.138/2018 e o Decreto 43.056/2022 e suas alterações (atualizado em 03/2022)
+        </Link>
+        <Link href="http://www.cap.seduh.df.gov.br/wp-content/uploads/2022/03/ANEXOS-Lei-no-6138-e-Decreto-43.056_2022.pdf" target="_blank">
+        ANEXOS – Lei nº 6138 e Decreto nº 43.056/2022 (atualizado em 03/2022)
+        </Link>
 
 
       </div>
